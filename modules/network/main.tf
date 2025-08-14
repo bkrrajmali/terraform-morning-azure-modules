028cd2647cc68a121d26a18d0c101dded5035c91
bkrrajmali <bkrraj@gmail.com>
2025-08-14 18:42:14 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -13,28 +13,4 @@ resource "azurerm_subnet" "this" {
   address_prefixes     = [var.subnet_prefix]
 }
 
-resource "azurerm_network_security_group" "this" {
-  name                = "nsg-${var.subnet_name}"
-  location            = var.location
-  resource_group_name = var.rg_name
-  tags                = var.tags
-}
 
-resource "azurerm_network_security_rule" "ssh" {
-  name                        = "Allow-SSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = var.allow_ssh_cidr
-  destination_address_prefix  = "*"
-  resource_group_name         = var.rg_name
-  network_security_group_name = azurerm_network_security_group.this.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "assoc" {
-  subnet_id                 = azurerm_subnet.this.id
-  network_security_group_id = azurerm_network_security_group.this.id
-}
