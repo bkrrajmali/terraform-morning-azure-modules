837a1bbc32214201917d53770279f65f94c7a14f
bkrrajmali <bkrraj@gmail.com>
2025-08-14 18:44:49 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "this" {
   location            = var.location
   resource_group_name = var.rg_name
   address_space       = var.address_space
-  tags                = var.tags
+
 }
 
 resource "azurerm_subnet" "this" {
