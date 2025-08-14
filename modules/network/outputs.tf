3d41f534bb2965db67c875fca65c19cd89b64404
bkrrajmali <bkrraj@gmail.com>
2025-08-14 18:46:46 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,3 @@
 output "vnet_name" { value = azurerm_virtual_network.this.name }
 output "subnet_id" { value = azurerm_subnet.this.id }
-output "nsg_id"    { value = azurerm_network_security_group.this.id }
+
